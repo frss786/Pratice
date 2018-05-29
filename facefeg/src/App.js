0c3ecb3c1c5bb4769b65556951e699aa3573e776
Xiaoyu Chen <xiaoyuchen@Xiaoyus-MacBook-Air.local>
2018-05-29 16:57:59 -0400

    Add background and more styles

--- facefeg/src/App.js
+++ facefeg/src/App.js
@@ -3,7 +3,29 @@ import logo from './logo.svg';
 import Nav from './Component/nav/Nav.js'
 import Logo from './Component/Logo/Logo.js'
 import ImageLink from './Component/ImageLink/ImageLink.js'
+import Rank from './Component/Rank/Rank.js'
+import Particles from 'react-particles-js'
 import './App.css';
+
+
+const particlesOp = {
+  particles: {
+    number:{
+      value: 30,
+      density:{
+        enable: true,
+        value_area: 100
+      }
+    },
+    // line_linked: {
+    //   shadow: {
+    //     enable: true,
+    //     color: "#3CA9D1",
+    //     blur: 5
+    //   }
+    // }
+  }
+}
 
 class App extends Component {
   //Add title to the page.
@@ -14,8 +36,13 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <Particles 
+              className='particles'
+              params={particlesOp}
+            />
         <Nav />
         <Logo />
+        <Rank />
         <ImageLink />
         {/*<FaceReg />*/}
       </div>
